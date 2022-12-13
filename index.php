2a1499ccd94e5daa928f5b2ae8d8af919292408e
MARTINA1102 <martinamiccoli11@gmail.com>
2022-12-13 21:03:24 +0100

    finito

--- index.php
+++ index.php
@@ -7,17 +7,39 @@
     <title>PHP Baswords</title>
 </head>
 <body>
-    <?php
-        $testo= "Nello studio della grammatica di un testo rientra l'analisi dei fattori psicologici inerenti alla percezione del ricevente; questo è evidente nei testi letterari ma anche nell'analisi delle lingue parlate (come i dialetti o i gerghi di comunità ristrette). Spesso le strutture sintattiche e le associazioni semantiche già scelte ripetono modelli già in uso nel patrimonio linguistico di una comunità.
+    <div>
+        <?php
+            $testo= "Nello studio della grammatica di un testo rientra l'analisi dei fattori psicologici inerenti alla percezione del ricevente; questo è evidente nei testi letterari ma anche nell'analisi delle lingue parlate (come i dialetti o i gerghi di comunità ristrette). Spesso le strutture sintattiche e le associazioni semantiche già scelte ripetono modelli già in uso nel patrimonio linguistico di una comunità.
+
+            A livello letterario questi schemi standardizzati sono analizzati e classificati dalla retorica classica, la quale riconosce procedimenti logici e moduli espressivi fissi e ripetuti, entro i quali si libera la creatività linguistica del soggetto parlante.
+            
+            La stilistica si occupa dell'analisi di libera creatività all'interno dei moduli standard di un testo. L'innovazione linguistica e l'espressività letteraria, infatti, nascono dalla rottura rispetto alla sintassi consolidata (vedi anche le figure di stile).";
+        ?>
+    </div>
+  
+    <div>
+        <?php echo "${testo}"; ?>
+    </div>
+    <h1>lunghezza testo: <?php echo strlen($testo); ?> parole</h1> 
+    <?php $testo_due = $_GET['testo_due']?>
+	<form method="get" action="">
+		<label for="testo_due">inserisci parola</label>
+		<input type="text" id="testo_due" name="testo_due">
+		<button>invio</button>
+    </form>
+    <?php 
+        $paragrafo = "Nello studio della grammatica di un testo rientra l'analisi dei fattori psicologici inerenti alla percezione del ricevente; questo è evidente nei testi letterari ma anche nell'analisi delle lingue parlate (come i dialetti o i gerghi di comunità ristrette). Spesso le strutture sintattiche e le associazioni semantiche già scelte ripetono modelli già in uso nel patrimonio linguistico di una comunità.
 
         A livello letterario questi schemi standardizzati sono analizzati e classificati dalla retorica classica, la quale riconosce procedimenti logici e moduli espressivi fissi e ripetuti, entro i quali si libera la creatività linguistica del soggetto parlante.
         
         La stilistica si occupa dell'analisi di libera creatività all'interno dei moduli standard di un testo. L'innovazione linguistica e l'espressività letteraria, infatti, nascono dalla rottura rispetto alla sintassi consolidata (vedi anche le figure di stile).";
+
+        $search = $_GET['testo_due'];
+        $sosituire = "***";
     ?>
-    <div><?php echo "${testo}"; strlen($testo) ?></div>
-    <form action="" method="get">
-        <label for="word">parola da censurare</label>
-        <input type="text" name="word" id="word">
-    </form>
+    <h4> parola corretta: <?php echo "${testo_due}";?>   
+        <h2><?php echo str_replace($search, $sosituire, $paragrafo);?></h2> 
+    </h4>  
+
 </body>
 </html>
